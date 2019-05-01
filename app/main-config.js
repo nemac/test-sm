dc8627f3f54f7c7e20487c7e4a6e07baf6cf00cd
Dave Michelson <daveism@gmail.com>
2019-05-01 10:54:22 -0400

    oops

--- app/main-config.js
+++ app/main-config.js
@@ -118,7 +118,7 @@ if( app.isProduction ) {
 loadJS('app/main-app.js');
 
 // Enable Google Analytics on storymaps.esri.com
-if (window.location.href.toLowerCase().indexOf("storymaps.esri.com") >= 0) {
+// if (window.location.href.toLowerCase().indexOf("storymaps.esri.com") >= 0) {
 	var _gaq = _gaq || [];
 	_gaq.push(['_setAccount', 'UA-139389990-1']);
 	_gaq.push(['_trackPageview']);
@@ -128,4 +128,4 @@ if (window.location.href.toLowerCase().indexOf("storymaps.esri.com") >= 0) {
 		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 	})();
-}
+// }
